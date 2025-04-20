061d2219c53e6936950dd6cd55673c13424fe8ea
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-04-21 02:38:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -13,41 +13,72 @@ function App() {
   const { user, logout } = useContext(AuthContext);
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
 
-  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  const handleNavCollapse = () => {
+    setIsNavCollapsed(!isNavCollapsed);
+  };
 
   return (
     <Router>
       <div className="min-vh-100 d-flex flex-column">
         {/* Sticky Navbar */}
         <header className="bg-dark text-white sticky-top shadow">
-          <div className="container py-3">
-            <div className="d-flex justify-content-between align-items-center">
-              <h1 className="mb-0 fs-3">🎓 Student DB</h1>
+          <div className="container py-3 d-flex flex-wrap align-items-center justify-content-between">
+            <h1 className="mb-0 fs-3">🎓 Student DB</h1>
 
-              {/* Toggle Button */}
-              <button
-                className="navbar-toggler d-md-none"
-                type="button"
-                onClick={handleNavCollapse}
-              >
-                <span className="navbar-toggler-icon bg-light"></span>
-              </button>
-            </div>
+            {/* Toggle Button for Mobile */}
+            <button
+              className="d-md-none border-0 bg-transparent"
+              type="button"
+              onClick={handleNavCollapse}
+              aria-label="Toggle navigation"
+            >
+              <span
+                style={{
+                  display: 'block',
+                  width: '24px',
+                  height: '2px',
+                  backgroundColor: 'black',
+                  margin: '5px 0',
+                }}
+              />
+              <span
+                style={{
+                  display: 'block',
+                  width: '24px',
+                  height: '2px',
+                  backgroundColor: 'black',
+                  margin: '5px 0',
+                }}
+              />
+              <span
+                style={{
+                  display: 'block',
+                  width: '24px',
+                  height: '2px',
+                  backgroundColor: 'black',
+                  margin: '5px 0',
+                }}
+              />
+            </button>
 
-            {/* Collapsible Nav */}
-            <div className={`mt-3 mt-md-0 ${isNavCollapsed ? 'd-none d-md-flex' : 'd-flex flex-column'} flex-md-row flex-wrap gap-2`}>
-              <Link to="/" className="btn btn-outline-light">Home</Link>
+            {/* Navigation Links */}
+            <nav
+              className={`d-md-flex flex-wrap gap-2 mt-3 mt-md-0 ${
+                isNavCollapsed ? 'd-none d-md-flex' : 'd-flex flex-column w-100'
+              }`}
+            >
+              <Link to="/" className="btn btn-outline-light w-100 w-md-auto">Home</Link>
               {user && (
                 <>
-                  <Link to="/students" className="btn btn-outline-light">Students</Link>
-                  <Link to="/courses" className="btn btn-outline-light">Courses</Link>
-                  <Link to="/enrollments" className="btn btn-outline-light">Enrollments</Link>
-                  <Link to="/marks" className="btn btn-outline-light">Marks</Link>
-                  <Link to="/profile" className="btn btn-outline-light">Profile</Link>
-                  <button onClick={logout} className="btn btn-outline-warning">Logout</button>
+                  <Link to="/students" className="btn btn-outline-light w-100 w-md-auto">Students</Link>
+                  <Link to="/courses" className="btn btn-outline-light w-100 w-md-auto">Courses</Link>
+                  <Link to="/enrollments" className="btn btn-outline-light w-100 w-md-auto">Enrollments</Link>
+                  <Link to="/marks" className="btn btn-outline-light w-100 w-md-auto">Marks</Link>
+                  <Link to="/profile" className="btn btn-outline-light w-100 w-md-auto">Profile</Link>
+                  <button onClick={logout} className="btn btn-outline-warning w-100 w-md-auto">Logout</button>
                 </>
               )}
-            </div>
+            </nav>
           </div>
         </header>
 
@@ -58,23 +89,43 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route
                 path="/students"
-                element={<ProtectedRoute><Students /></ProtectedRoute>}
+                element={
+                  <ProtectedRoute>
+                    <Students />
+                  </ProtectedRoute>
+                }
               />
               <Route
                 path="/courses"
-                element={<ProtectedRoute><Courses /></ProtectedRoute>}
+                element={
+                  <ProtectedRoute>
+                    <Courses />
+                  </ProtectedRoute>
+                }
               />
               <Route
                 path="/enrollments"
-                element={<ProtectedRoute><Enrollments /></ProtectedRoute>}
+                element={
+                  <ProtectedRoute>
+                    <Enrollments />
+                  </ProtectedRoute>
+                }
               />
               <Route
                 path="/marks"
-                element={<ProtectedRoute><Marks /></ProtectedRoute>}
+                element={
+                  <ProtectedRoute>
+                    <Marks />
+                  </ProtectedRoute>
+                }
               />
               <Route
                 path="/profile"
-                element={<ProtectedRoute><Profile /></ProtectedRoute>}
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
               />
             </Routes>
           </div>
